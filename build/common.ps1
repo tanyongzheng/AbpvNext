6e5ca7ceb42da7c1e156b3a43009b97d43c0889d
tanyongzheng <tanyongzheng@126.com>
2021-10-16 22:56:44 +0800

    feat:添加Github Action配置

---
+++ build/common.ps1
@@ -0,0 +1,23 @@
+$full = $args[0]
+
+# 公共路径 
+
+$rootFolder = (Get-Item -Path "./" -Verbose).FullName
+
+# 开发模式下的解决方案列表，主要包含类库项目和部分样例项目，.sln文件所在路径
+$solutionPaths = @(
+		"../templates",
+		"../samples/WebApiDemo"
+	)
+
+if ($full -eq "-f")
+{
+	# 完整构建需要的其他解决方案，譬如WPF项目
+	$solutionPaths += (
+		"../samples/ConsoleDemo"
+	) 
+}else{ 
+	Write-host ""
+	Write-host ":::::::::::::: !!! You are in development mode !!! ::::::::::::::" -ForegroundColor red -BackgroundColor  yellow
+	Write-host "" 
+} 
